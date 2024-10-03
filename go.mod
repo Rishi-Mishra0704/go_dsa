ec70d43e901a049542973886ef9161df8b683e5f
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-10-03 13:33:13 +0530

    updates

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Rishi-Mishra0704/go_dsa
+
+go 1.22.2
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
+import "fmt"
+
 
 func main(){
-// integer list  
-intList := CreateIntList([]int{1, 2, 3, 4, 5})
-PrintLinkedList(intList) // Output: 1 -> 2 -> 3 -> 4 -> 5 -> nil
-// string list
+  // integer list  
+  fmt.Println("Integer List:")
+  intList := CreateIntList([]int{1, 2, 3, 4, 5})
+  PrintLinkedList(intList) // Output: 1 -> 2 -> 3 -> 4 -> 5 -> nil
+  // string list
+  fmt.Println("String List:")
   strList := CreateStringList([]string{"apple", "banana", "cherry"})
   PrintLinkedList(strList) // Output: apple -> banana -> cherry -> nil
 
